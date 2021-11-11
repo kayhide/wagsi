bd5090eb01269ad5d0bc5afdf0a5648b4e57d791
Hideaki Kawai <kayhide@gmail.com>
2021-11-11 16:02:54 +0900

    Beat

--- src/LiveCodeHere/Wagged.purs
+++ src/LiveCodeHere/Wagged.purs
@@ -6,6 +6,6 @@ import WAGSI.Plumbing.Types (WhatsNext)
 wag :: WhatsNext
 wag =
   make 1.0
-    { earth: s ""
+    { earth: s "bassdm <hh [hh hh hh hh]> [bassdm:2 bassdm:2] hh27"
     , title: "i m a k e n o i s e"
     }
